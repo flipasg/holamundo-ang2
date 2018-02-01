399df37c26fa8eed1421e521fb8c4551ad5c6cca
Iker Garcia <iker.garcia@geograma.com>
2018-02-01 15:55:09 +0100

    module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
